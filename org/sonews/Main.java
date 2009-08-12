a3bbbb37356d71346ae000bfa26c0e50f0f6b7bb
cli <devnull@localhost>
2009-08-12 16:39:04 +0200

    sonews/1.0.1: Disable strict parsing of email addresses.

--- org/sonews/Main.java
+++ org/sonews/Main.java
@@ -48,7 +48,7 @@ public final class Main
   }
 
   /** Version information of the sonews daemon */
-  public static final String VERSION = "sonews/1.0.0";
+  public static final String VERSION = "sonews/1.0.1";
   public static final Date   STARTDATE = new Date();
   
   /**
