310fde314a9eb8916a488f4d69e7ef6c025e5c3b
Christian Lins <christian@lins.me>
2012-02-28 17:13:16 +0100

    Start documenting LocalStorage mechanism.

--- src/org/sonews/storage/impl/LocalStorageProvider.java
+++ src/org/sonews/storage/impl/LocalStorageProvider.java
@@ -4,12 +4,26 @@ import org.sonews.storage.Storage;
 import org.sonews.storage.StorageBackendException;
 import org.sonews.storage.StorageProvider;
 
+/**
+ * Provider for @see{LocalStorage}.
+ * LocalStorage is a simple method for storing news in the local filesystem of
+ * the server running SONEWS.
+ * The news saved one file per news and are properly indexed for faster access.
+ * Although it is not recommended to use LocalStorage for large installations
+ * as the performance will decrease with growing numbers of news stored.
+ * Additionally, there are hard limits dependending on the underlying OS and
+ * filesystem.
+ *
+ * Directory structure:
+ * $BASE$: Base directory of the LocalStorage, e.g. /var/share/sonews/stor0
+ * $BASE$/news/: contains the news mails, one file per news named by its Message-ID
+ * $BASE$/index/: contains index files referencing the files in ../news
+ */
 public class LocalStorageProvider implements StorageProvider {
 
 	@Override
 	public boolean isSupported(String uri) {
-		// TODO Auto-generated method stub
-		return false;
+		return uri.startsWith("sonews:local");
 	}
 
 	@Override
