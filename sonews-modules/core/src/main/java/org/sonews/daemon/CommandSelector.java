8d3a8631481698dd15b3fe9a7f61db9d863b9b73
Christian Lins <christian@lins.me>
2014-12-04 22:42:25 +0100

    Cosmetic changes

--- sonews-modules/core/src/main/java/org/sonews/daemon/CommandSelector.java
+++ sonews-modules/core/src/main/java/org/sonews/daemon/CommandSelector.java
@@ -15,12 +15,14 @@
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.sonews.daemon;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
+
 import javax.annotation.PostConstruct;
 
 import org.sonews.daemon.command.Command;
